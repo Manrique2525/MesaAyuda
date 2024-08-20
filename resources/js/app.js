081b1e86eec466ec8734dc472bf6ae268b23ba04
Manrique2525 <manriquemontero25@gmail.com>
2024-08-19 21:04:26 -0600

    inciando proyecto de mesa de ayuda con diseño de una plantilla de boostrap, se añadio el header y el aside, register y login

---
+++ resources/js/app.js
@@ -0,0 +1,12 @@
+// Importa el archivo bootstrap.js que contiene la configuración de Bootstrap
+import './bootstrap';
+
+// Importa Bootstrap JS
+import 'bootstrap';
+
+// Importa AlpineJS
+import Alpine from 'alpinejs';
+
+// Configura AlpineJS
+window.Alpine = Alpine;
+Alpine.start();
